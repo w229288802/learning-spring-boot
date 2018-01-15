2f8080dd0f9f55d0c216fb1ae103440454c97220
welge <229288802@qq.com>
2018-01-15 23:25:20 +0800

    Modified

--- chapter6-properties/src/main/java/com/example/demo/Bootstrap.java
+++ chapter6-properties/src/main/java/com/example/demo/Bootstrap.java
@@ -1,34 +1,27 @@
 package com.example.demo;
 
 import com.example.demo.service.properties.CommonProperties;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.Properties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CommonProperties.class)
 @PropertySources(
 		@PropertySource("classpath:/${spring.profiles.active}/common.properties")
 )
-public class Application {
+public class Bootstrap {
 
 	public static void main(String[] args) throws IOException {
 		//--spring.profiles.active=prod
-		SpringApplication springApplication = new SpringApplication(Application.class);
+		SpringApplication springApplication = new SpringApplication(Bootstrap.class);
 		springApplication.setAddCommandLineProperties(false);
 		springApplication.run(args);
-		//SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Bootstrap.class, args);
 		//Properties properties = PropertiesLoaderUtils.loadAllProperties("dev/common.properties");
 		//log.info(properties.toString());
 	}
--- chapter6-properties/src/main/java/com/example/demo/service/properties/CommonProperties.java
+++ chapter6-properties/src/main/java/com/example/demo/service/properties/CommonProperties.java
@@ -9,6 +9,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties(prefix = "app")
 public class CommonProperties {
+    /**
+     * 应用名字
+     */
     private String name;
 }
 
