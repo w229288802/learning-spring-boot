68ff344d6c9cadd33528b4ac13d0f934bd73afd3
welge <229288802@qq.com>
2018-01-16 00:19:42 +0800

    Modified

--- chapter7-multiple-datasource/src/main/java/com/example/ApplicationTests.java
+++ chapter7-multiple-datasource/src/main/java/com/example/ApplicationTests.java
@@ -7,14 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Chapter7Application.class)
-@SuppressWarnings("all")
+@SpringBootTest
 public class ApplicationTests {
 
 	@Autowired
