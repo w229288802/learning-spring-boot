6da3a69a9c6936e9a61220cd9c90749c67120d96
wuweijie <484313202@qq.com>
2018-01-16 16:39:43 +0800

    Modified

--- chapter10-async/src/test/java/com/example/demo/Chapter10AsyncApplicationTests.java
+++ chapter10-async/src/test/java/com/example/demo/Chapter10AsyncApplicationTests.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.scheduling.SchedulingService;
+import com.example.demo.scheduling.AsyncService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class Chapter10AsyncApplicationTests {
 
 	@Autowired
-	private SchedulingService schedulingService;
+	private AsyncService schedulingService;
 
 	@Test
 	public void contextLoads() throws Exception {
