01885a2c40a9bc25be0ee6e2b80a2a64385a8209
wuweijie <484313202@qq.com>
2018-01-16 18:11:33 +0800

    Modified

--- chapter0-spring-feature/src/test/java/com/example/demo/Chapter0SpringFeatureApplicationTests.java
+++ chapter0-spring-feature/src/test/java/com/example/demo/Chapter0SpringFeatureApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
